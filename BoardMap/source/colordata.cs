37177c62b53f355b3b38456b90ee9e2fbdd74d65
Daniel <danielmorillo249@hotmail.com>
2019-08-20 14:28:07 +0200

    changed colordata to not throw exception for compatibility

--- BoardMap/source/colordata.cs
+++ BoardMap/source/colordata.cs
@@ -22,6 +22,7 @@ namespace BoardMap
         {
             // check bounds
             if(pos_x < 0 || pos_x > Width || pos_y < 0 || pos_y > Height) {
+                return data[0];
                 throw new IndexOutOfRangeException("ColorData out of bounds");
             }
             // else return from coordinates

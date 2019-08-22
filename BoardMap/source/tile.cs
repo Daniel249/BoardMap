a47989d54b6e81c14d2ecf3bb3b58f9b36507809
Daniel <danielmorillo249@hotmail.com>
2019-08-22 16:35:46 +0200

    added tile class and implemented reader on defreader

---
+++ BoardMap/source/tile.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardMap.source
+{
+    // properties loaded from definitions.csv
+    class Tile
+    {
+        // tile id. from 1 to 13k
+        public int ID { get; private set; }
+        // tile color
+        public Color color { get; private set; }
+        // is land tile
+        public bool isLand { get; private set; }
+        // is coastal
+        public bool isCoastal { get; private set; }
+
+
+
+
+
+        // constructor 
+        public Tile(int _id, Color _color, bool _isLand, bool _isCoastal) {
+            ID = _id;
+            color = _color;
+            isLand = _isLand;
+            isCoastal = _isCoastal;
+        }
+
+    }
+}

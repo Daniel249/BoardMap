aab1577905b36165c66a5b12b07807d71852d86b
Daniel <danielmorillo249@hotmail.com>
2019-08-25 18:13:06 +0200

    added search and get first tile

--- BoardMap/source/Landscape/landscape.cs
+++ BoardMap/source/Landscape/landscape.cs
@@ -26,13 +26,40 @@ namespace BoardMap.LandscapeNS
         public Dictionary<Color, Tile> tiles { get; private set; }
 
 
+        // get tile from dictionary
+        public Tile searchTile(Color _color) {
+            // if found in dictionary return
+            Tile tile;
+            if (tiles.TryGetValue(_color, out tile)) {
+                return tile;
+            } else {
+                return tiles.First().Value;
+            }
+            // else return
+        }
+
+
+        // check tile with most textures
+        public Tile getmaxTextures() {
+            
+            // get random tile
+            Tile currentTile = tiles.First().Value;// = new Tile();
+            // to return actual max
+            foreach(KeyValuePair<Color, Tile> entry in tiles) {
+                Tile _tile = entry.Value;
+                if (currentTile.textures.Count < _tile.textures.Count) {
+                    currentTile = _tile;
+                }
+            }
+            return currentTile;
+        }
 
         // constructor
         public Landscape(Texture2D _texture) {
             // init loader with map texture
             TileLoader tloader = new TileLoader(_texture);
             // run loader and set tiles
-            tiles = tloader.processData(40);
+            tiles = tloader.processMap(40);
         }
         // create landscale without loading tiles
         // init stuff to 0

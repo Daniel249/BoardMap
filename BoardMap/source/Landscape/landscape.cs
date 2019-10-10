233d2322c543850a9d61dfd3086f8b59941df265
Daniel <danielmorillo249@hotmail.com>
2019-10-10 21:14:07 +0200

    generalized diffcolor state drawing and removed p key

--- BoardMap/source/Landscape/landscape.cs
+++ BoardMap/source/Landscape/landscape.cs
@@ -28,11 +28,9 @@ namespace BoardMap.LandscapeNS
 
         // map modes
         // change map mode with keyboard
+        #region
 
-        // tile counter for R
-        int counter = 1;
-
-        public Color waterColor = new Color(163, 192, 245); //Color.CornflowerBlue; // = new Color(128, 255, 255);
+        public Color waterColor = Color.White;// new Color(163, 192, 245); //Color.CornflowerBlue; // = new Color(128, 255, 255);
         public void changeMapMode(KeyboardState lastState) {
             // get currentState
             KeyboardState currentState = Keyboard.GetState();
@@ -63,25 +61,10 @@ namespace BoardMap.LandscapeNS
             } else if (listKeys.Contains(Keys.W)) {
 
                 // W: draw states 
-                for (int i = 1; i < definitions.Length; i++) {
-                    if (definitions[i].isLand) {
-                        // make color less blue
-                        Color stateColor = definitions[i].state.color;
-                        // calc blue fraction as 1/4
-                        byte blueFraction = (byte)(stateColor.B / 4);
-                        // add to R and G and take from B
-                        stateColor.R += blueFraction;
-                        stateColor.G -= blueFraction;
-                        stateColor.B -= blueFraction;
-                        if(stateColor.B < 0) {
-                            stateColor.B = 0;
-                        }
-                        // draw
-                        definitions[i].drawTile(stateColor, canvas);
-                    } else {
-                        // definitions[i].drawTile(waterColor, canvas);
-                    }
-                }
+                for(int i = 0; i < states.Length; i++) {
+                    State _state = states[i];
+                    drawStateDiffcolor(_state, _state.color, 0.3f, 0.3f, -1f, canvas);
+                } 
             } else if (listKeys.Contains(Keys.E)) {
 
                 // E: draw countries
@@ -114,15 +97,6 @@ namespace BoardMap.LandscapeNS
                     }
                 }
 
-            } else if (listKeys.Contains(Keys.P)) {
-
-                // P: draw only one tile
-                // draw a tile on canvas
-                int howMany = 10;
-                for(int i = 0; i < howMany; i++) {
-                    definitions[i + counter].drawTile(canvas);
-                }
-                counter += howMany;
             } else {
                 // if no match dont print blank canvas
                 return;
@@ -132,7 +106,7 @@ namespace BoardMap.LandscapeNS
             // update texture colodata
             Tile.frameReference.updateTexture(canvas);
         }
-
+        #endregion
 
         // get tile from dictionary
         public Tile searchTile(Color _color) {
@@ -141,43 +115,39 @@ namespace BoardMap.LandscapeNS
             if (tiles.TryGetValue(_color, out tile)) {
                 return tile;
             } else {
+                // else return null tile
                 return definitions[0];
             }
-            // else return
         }
 
-        public void drawLightState(Tile _tile, Color _color) {
-            // get state
-            State lighttState = _tile.state;
-
+        // rgb parameters as fraction closer to 255 relative to actual state color
+        // like this: difR = 1/5 then new color is 4/5 the way to 255
+        // can be negative to get far from 255
+        public void drawStateDiffcolor(State lighttState, Color _color, float difR, float difG, float difB, ColorData<Color> currentTexture) {
             // if null is ocean probably
-            if(lighttState != null) {
+            if (lighttState != null) {
                 // get color and make lighter
                 // set to fraction of distance to 255 from lightState.color
                 Color lightColor = new Color(
-                    255 - (255 - _color.R) * 4 / 5,
-                    255 - (255 - _color.G) * 4 / 5,
-                    255 - (255 - _color.B) * 4 / 5);
 
-                // get data from frame once
-                ColorData<Color> currentTexture = Tile.frameReference.colorData.getCopy();
+                    (int)(255 - (255 - _color.R) * (1 - difR) ),
+                    (int)(255 - (255 - _color.G) * (1 - difG) ),
+                    (int)(255 - (255 - _color.B) * (1 - difB) ));
+
                 // draw each tile in state with light color
                 for (int i = 0; i < lighttState.tiles.Length; i++) {
                     lighttState.tiles[i].drawTile(lightColor, currentTexture);
                 }
-                // redraw selected tile with state color
-                _tile.drawTile(_color, currentTexture);
-                // update texture
-                Tile.frameReference.updateTexture(currentTexture);
             }
-
         }
 
+
+        // draw 
         public void drawCountry(Tile _tile) {
             // get state
             State _state = _tile.state;
 
-            // if null is ocean probably
+            // if null, is ocean probably
             if (_state != null) {
                 // get country color
                 Color color = _tile.state.country.color;
@@ -188,6 +158,12 @@ namespace BoardMap.LandscapeNS
                 for(int state_count = 0; state_count < _tile.state.country.states.Count; state_count++) {
                     // draw each tile in state with 
                     State currentState = _state.country.states[state_count];
+
+                    // dont draw if is selected state. it will be drawn later
+                    if (currentState == _tile.state) {
+                        continue;
+                    }
+                    // loop through tiles
                     for (int i = 0; i < currentState.tiles.Length; i++) {
                         // if land
                         if(currentState.tiles[i].isLand) {
@@ -197,15 +173,13 @@ namespace BoardMap.LandscapeNS
 
                 }
 
-                // redraw selected tile's state
-                // draw lighter
-                color = new Color(
-                    255 - (255 - color.R) * 4 / 5,
-                    255 - (255 - color.G) * 4 / 5,
-                    255 - (255 - color.B) * 4 / 5);
-                for (int i = 0; i < _state.tiles.Length; i++) {
-                    _state.tiles[i].drawTile(color, currentTexture);
-                }
+                float relativeDiff = 1f/5f;
+                // draw state lighter
+                drawStateDiffcolor(_tile.state, _tile.state.country.color, relativeDiff, relativeDiff, relativeDiff, currentTexture);
+                // drawLightState(_tile, _tile.state.color, currentTexture);
+                // redraw selected tile with actual state color
+                _tile.drawTile(_tile.state.color, currentTexture);
+
                 // update texture
                 Tile.frameReference.updateTexture(currentTexture);
             }
@@ -257,6 +231,8 @@ namespace BoardMap.LandscapeNS
             DefReader dreader = new DefReader();
             // init reader for countries.txt
             LandReader lreader = new LandReader();
+            // init reader for states folder
+            StateReader sreader = new StateReader(this); // needs initialized tiles 
 
             // run loader and set tiles
             tiles = tloader.processMap(50);
@@ -264,9 +240,6 @@ namespace BoardMap.LandscapeNS
             definitions = dreader.processFile(tiles);
             // read file and set countries
             countries = lreader.processFile();
-
-            // init reader for states folder
-            StateReader sreader = new StateReader(this); // needs initialized tiles 
             // read files and set states
             states = sreader.processFile();
             State.initStates(states);
@@ -277,7 +250,7 @@ namespace BoardMap.LandscapeNS
         }
         // create landscale without loading tiles
         // init stuff to 0
-        public void genLandscape() {
+        void genLandscape() {
             tiles = new Dictionary<Color, Tile>();
             definitions = new Tile[0];
             states = new State[0];

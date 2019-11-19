799a26ae6fc14cadbafb5f46a6e6f331c05275e7
Daniel <danielmorillo249@hotmail.com>
2019-11-19 23:18:56 +0100

    added calcPrice and calc production and applied to resolveMarket

--- BoardMap/source/Economy/market.cs
+++ BoardMap/source/Economy/market.cs
@@ -12,12 +12,12 @@ namespace BoardMap.Economy
         // ID of good
         int ID;
         // total subscribed demmand. 1/1000
-        long aggDemand;
+        double aggDemand;
         // list of suppliers
         public List<Firm> Supply { get; private set; }
 
         // price from last tick for pops' bundle calculations
-        public int lastPrice { get; private set; }
+        public double lastPrice { get; private set; }
         
         // main market method
         public void resolveMarket() {
@@ -25,7 +25,20 @@ namespace BoardMap.Economy
             for(int i = 0; i < Supply.Count; i++) {
                 Supply[i].Think();
             }
-            // add all firms' supply
+
+            // calc all firms' supply
+            double[] production = new double[Supply.Count];
+            double totalProduction = 0;
+
+            // calc new price
+            double newPrice = calcPrice();
+            // calc productions
+            totalProduction = hypoPrice(newPrice, production);
+
+            // add production to state gdp
+            for (int i = 0; i < Supply.Count; i++) {
+                Supply[i].operate(newPrice);
+            }
 
             // find spot
 
@@ -33,13 +46,48 @@ namespace BoardMap.Economy
 
             // reset aggregate demand for next cycle
             aggDemand = 0;
+            lastPrice = newPrice;
+        }
+
+        // calc with hypothetical price. store in production array and return total production
+        double hypoPrice(double price, double[] production) {
+            // init return
+            double totalProduction = 0;
+            // loop through supply firms
+            for(int i = 0; i < Supply.Count; i++) {
+                // calc firm production and add to total
+                totalProduction += Supply[i].operate(price);
+            }
+            // return total
+            return totalProduction;
+        }
+
+        double calcPrice() {
+            double suma = 0;
+            // loop through supply firms
+            for (int i = 0; i < Supply.Count; i++) {
+                Firm firm = Supply[i];
+                // calc
+                double klammern = firm.laborProductivity * firm.laborIntensity / firm.wage;
+                double potenz = firm.laborIntensity / (1 - firm.laborIntensity);
+
+                // calc and add 
+                double temp = Math.Pow(klammern, potenz);
+                double hold = firm.laborProductivity * temp;
+                suma += hold;
+            }
+            // use revenue
+
+            // return total
+            double potenzz = 1f - Supply[0].laborIntensity;
+            return Math.Pow( aggDemand / suma, potenzz);
         }
 
         // receive order from marketplace
-        public void addDemand(int demandValue) {
+        public void addDemand(double demandValue) {
             aggDemand += demandValue;
         }
-
+    
 
         // constructor
         public Market(int _id) {
